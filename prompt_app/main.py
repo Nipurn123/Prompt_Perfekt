4fb3c841749217bd3ccffe536e40a7f1082f5019
Nipurn123 <154745876+Nipurn123@users.noreply.github.com>
2024-05-13 11:38:31 +0530

    main.py

--- prompt_app/main.py
+++ prompt_app/main.py
@@ -14,7 +14,7 @@ class ChatApp:
        
         self.model_name = st.sidebar.selectbox("Choose Generative AI Model", ["Nipurn's LLM", "Gemini","Ollama"])
         if self.model_name == "Nipurn's LLM":
-            self.api_key = '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'
+            self.api_key = 'sk-proj-1a5R6OdbKWOk8WW4tCsmT3BlbkFJu1FxVl1bnqwSAZBYjJxZ'
         elif self.model_name == "Gemini":
             self.api_key = st.sidebar.text_input("Enter your Google API Key:") # Sample API Key
 
